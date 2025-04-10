61f5b1031e4744519825649ea5000b12c0fae2f6
simonmonday01 <simonmonday209@gmail.com>
2025-04-10 03:17:49 -0700

    second commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+"use client"
+import { useEffect, useState } from "react"
 
 
-export default function home() {
-  return(
-    <main>landing page</main>
-  )
+export default functionsHome (){
+const [count,setcount] =useState(0
+
+return (
+  <main className="flex flex-col gap-20 items-center justify-center px-20 py-20">
+  <p className="text-4x1 font-bold">count : {count}</p>
+  <button onClick={() => setcount(count+1)} className="w-[200px] h-[50px] text-white bg-blue-400">Increment</button>
+  <button onClick={() => setcount(count+1)} className="w-[200px] h-[50px] text-white bg-blue-400">Decrement</button>
+  </main>
+)
 }
